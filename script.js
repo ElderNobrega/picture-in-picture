187f17da19c15dd761b46ae30e703e83e5d42c04
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-08-24 16:53:54 -0400

    styling

---
+++ script.js
@@ -0,0 +1,16 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+// prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    something
+  } catch (error) {
+    console.log('error here', error)
+  }
+}
+
+// on load
+
+selectMediaStream()
